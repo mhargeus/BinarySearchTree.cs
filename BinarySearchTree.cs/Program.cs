f52b654106a9378209a21ac7015979f07583a1cd
Mikael Hargéus <mikael.hargeus@live.se>
2022-07-01 10:37:06 +0200

    All is working

--- BinarySearchTree.cs/Program.cs
+++ BinarySearchTree.cs/Program.cs
@@ -1,5 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+//Mitt träd är inte speciellt effektivt eftersom det saknar balansen som krävs. Beroende på vilket värde som implementeras först kan "Root" bli ett
+//	extremfall som inte är mer effektiv än en vanlig lista, då hela vägen till sista noden blir en vandring åt höger t.ex. Man tappar alltså poängen med Divide and Conquer sökning.
+//	Worst case är en lista 
+//
+//
+//
+//
+//
+//
 
 
 BinarySearchTree<int> testList = new BinarySearchTree<int>();
@@ -7,6 +16,14 @@ testList.Insert(1);
 testList.Insert(2);
 testList.Insert(3);
 testList.Insert(5);
+testList.Insert(19);
+testList.Insert(0);
+testList.Insert(6);
+testList.Insert(22);
+testList.Insert(13);
+testList.Insert(4);
+testList.Insert(32);
+testList.Insert(33);
 
 Console.WriteLine(testList.Count());
 Console.WriteLine(testList.Exists(2));
@@ -15,6 +32,8 @@ Console.WriteLine(testList.Exists(3));
 Console.WriteLine(testList.Exists(4));
 Console.WriteLine(testList.Exists(5));
 Console.WriteLine(testList.Exists(0));
+
+testList.Print();
 
 
 public interface BST_G<T> where T : IComparable<T>
@@ -39,24 +58,25 @@ public class BinarySearchTree<T> : BST_G<T> where T : IComparable<T>
 
 	public void Insert(T value)
 	{
-		var currentNode = Root; 
 		var newNode = new Node<T>(value);
 
 
 		if (Exists(value)) return; //Dubblettkontroll
 		if (Root == null) { Root = newNode; counter++; return; } //Skapar Root om träd ej påbörjat
 
+		var currentNode = Root;
+
 		while (true)
 		{
 			if (currentNode.Data.CompareTo(value) > 0)  //Jämför med root, om den är lägre gå vänster, högre gå höger.
 			{
-				if (currentNode.LeftChild == null) { currentNode.LeftChild = newNode; counter++; } //om nod är tom, skapa ny
-				else { currentNode = newNode.LeftChild; counter++; return;}; // om nod ej är tom, gå ett steg till
+				if (currentNode.LeftChild == null) { currentNode.LeftChild = newNode; counter++; return; } //om nod är tom, skapa ny
+				{ currentNode = currentNode.LeftChild; }; // om nod ej är tom, gå ett steg till
 			}
 			else
 			{
-				if (currentNode.RightChild == null) { currentNode.RightChild = newNode; counter++; }//om nod är tom, skapa ny
-				else { currentNode = newNode.RightChild; counter++; return;}; // om nod ej är tom, gå ett steg till
+				if (currentNode.RightChild == null) { currentNode.RightChild = newNode; counter++; return; }//om nod är tom, skapa ny
+				{ currentNode = currentNode.RightChild; }; // om nod ej är tom, gå ett steg till
 			}
 		}
 	}
@@ -78,6 +98,53 @@ public class BinarySearchTree<T> : BST_G<T> where T : IComparable<T>
 		return false;
 
     }
+
+	public void Print()
+	{
+		Queue<Node<T>?> nodes = new Queue<Node<T>?>();
+		Queue<Node<T>?> newNodes = new Queue<Node<T>?>();
+		nodes.Enqueue(Root);
+		int depth = 0;
+
+		bool exitCondition = false;
+		while (nodes.Count > 0 && !exitCondition)
+		{
+			depth++;
+			newNodes = new Queue<Node<T>?>();
+
+			string xs = "[";
+			foreach (var maybeNode in nodes)
+			{
+				string data = maybeNode == null ? " " : "" + maybeNode.Data;
+				if (maybeNode == null)
+				{
+					xs += "_, ";
+					newNodes.Enqueue(null);
+					newNodes.Enqueue(null);
+				}
+				else
+				{
+					Node<T> node = maybeNode;
+					string s = node.Data.ToString();
+					xs += s.Substring(0, Math.Min(4, s.Length)) + ", ";
+					if (node.LeftChild != null) newNodes.Enqueue(node.LeftChild);
+					else newNodes.Enqueue(null);
+					if (node.RightChild != null) newNodes.Enqueue(node.RightChild);
+					else newNodes.Enqueue(null);
+				}
+			}
+			xs = xs.Substring(0, xs.Length - 2) + "]";
+
+			Console.WriteLine(xs);
+
+			nodes = newNodes;
+			exitCondition = true;
+			foreach (var m in nodes)
+			{
+				if (m != null) exitCondition = false;
+			}
+		}
+	}
 }
 
 public class Node<T>
@@ -102,5 +169,5 @@ public class Node<T>
 		int right = (RightChild == null) ? 0 : RightChild.GetBalance() + 1;
 		return right - left;
 	}
-
 }
+
